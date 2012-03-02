cb85fdc031f4a5e5c1fa4683a65c8a08f44d1b3a
Yao Wei (魏銘廷) <mwei@lxde.org>
2012-03-03 00:09:01 +0800

    update ip rules in config.example.js

--- config.example.js
+++ config.example.js
@@ -17,9 +17,17 @@ exports.mongoHost = "localhost";
 exports.mongoPort = 27017;
 
 // valid ip rule
-ipRegex = /^140.121.\d+.\d+$/;
 exports.ipRule = function(ip) {
-  return ipRegex.exec(ip);
+  var s = ip.split(".");
+  var c = [];
+  c[0] = parseInt(s[0]);
+  c[1] = parseInt(s[1]);
+  c[2] = parseInt(s[2]);
+  c[3] = parseInt(s[3]);
+  return (c[0] == 140 &&
+          c[1] == 121 &&
+          c[2] > 1 && c[2] < 255 &&
+          c[3] > 1 && c[3] < 255);
 }
 
 // banning rule
